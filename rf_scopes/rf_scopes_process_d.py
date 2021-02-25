3b94c79d008d19f40de0e3f4a89d7f65a72083c2
femanov <femanov@gmail.com>
2021-02-25 17:15:22 +0700

    - some changes in widgets and channels - initial version of RF processing service

--- rf_scopes/rf_scopes_process_d.py
+++ rf_scopes/rf_scopes_process_d.py
@@ -7,14 +7,13 @@ class RFScopesProc:
     def __init__(self):
         self.shot_c = cda.IChan("cxhw:15.l_timer.shot")
 
-        self.scopes_update_c = cda.IChan("cxhw:15.adc250_9c.marker")
+        self.scopes_update_c = cda.IChan("cxhw:15.adc250_9a.marker")
         self.scopes_update_c.valueMeasured.connect(self.shot_done)
         self.shot_c.setValue(1)
 
-
     def shot_done(self, chan):
         self.shot_c.setValue(1)
-
+        print("shot done")
 
 class RFScopesProcService(CXService):
     def main(self):
