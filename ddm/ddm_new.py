3f9112f39cc43e7ebe183df980b659a43e61fd71
femanov <femanov@gmail.com>
2021-11-16 17:59:03 +0700

    some updates

--- ddm/ddm_new.py
+++ ddm/ddm_new.py
@@ -4,7 +4,8 @@ from cxwidgets.aQt.QtWidgets import QLabel, QApplication
 from cxwidgets.aQt.QtCore import Qt
 from cxwidgets.aQt import QtGui
 
-from cxwidgets import CXSwitch, CXSpinBox, CXPushButton, CXEventLed, CXTextComboBox, CXLineEdit, CXProgressBar, HLine, BaseGridW
+from cxwidgets import CXSwitch, CXSpinBox, CXPushButton, CXEventLed, CXTextComboBox, CXLineEdit, CXProgressBar,\
+    HLine, BaseGridW, CXIntComboBox, CXIntLabel
 from training_ctl_widget import TrainingCtlW
 
 from acc_ctl.mode_defs import mode_colors
@@ -20,33 +21,51 @@ class InjExtCtl(BaseGridW):
         grid = self.grid
         grid.addWidget(QLabel("injection/extraction control"), 0, 0, 1, 4, Qt.AlignHCenter)
 
-        grid.addWidget(QLabel("e-shots"), 1, 0)
+        grid.addWidget(QLabel("linac run-mode"), 1, 0, 1, 1, Qt.AlignRight)
+        self.linac_mode_cb = CXIntComboBox(cname='canhw:19.syn_ie4.mode', values={0: 'continuous', 1: 'counter'})
+        grid.addWidget(self.linac_mode_cb, 1, 1)
+
+        grid.addWidget(QLabel("beam switch"), 2, 0, 1, 1, Qt.AlignRight)
+
+        # turning magnet control
+        grid.addWidget(QLabel("beam dump"), 3, 0, 1, 1, Qt.AlignRight)
+        self.linac_dump_state = CXIntLabel(cname='canhw:21.turnmag.cur_direction',
+                                           values={2: '->Dump', 1: '->Ring', 3: 'between'},
+                                           colors={2: '#FF0000', 1: '#00FF00', 3: '#FFFF00'})
+        grid.addWidget(self.linac_dump_state, 3, 1)
+
+        self.dump_close_btn = CXPushButton('toDump', cname='canhw:21.turnmag.go_bury_max')
+        grid.addWidget(self.dump_close_btn, 3, 2)
+        self.dump_open_btn = CXPushButton('toRing', cname='canhw:21.turnmag.go_ring_max')
+        grid.addWidget(self.dump_open_btn, 3, 3)
+
+        grid.addWidget(QLabel("e-shots"), 4, 0, 1, 1, Qt.AlignRight)
         self.sb_eshots = CXSpinBox(cname='cxhw:0.ddm.eshots')
-        grid.addWidget(self.sb_eshots, 1, 1)
+        grid.addWidget(self.sb_eshots, 4, 1)
 
-        grid.addWidget(QLabel("p-shots"), 1, 2)
+        grid.addWidget(QLabel("p-shots"), 4, 2, 1, 1, Qt.AlignRight)
         self.sb_eshots = CXSpinBox(cname='cxhw:0.ddm.pshots')
-        grid.addWidget(self.sb_eshots, 1, 3)
+        grid.addWidget(self.sb_eshots, 4, 3)
 
-        grid.addWidget(QLabel("particles"), 2, 0)
+        grid.addWidget(QLabel("particles"), 5, 0, 1, 1, Qt.AlignRight)
         self.cb_particles = CXTextComboBox(cname='cxhw:0.ddm.particles', values=['e', 'p'],
                                            icons=[script_path + '/img/electron.png', script_path + '/img/positron.png'])
-        grid.addWidget(self.cb_particles, 2, 1)
+        grid.addWidget(self.cb_particles, 5, 1)
 
         self.b_inject = CXPushButton('Inject', cname='cxhw:0.ddm.inject')
-        grid.addWidget(self.b_inject, 2, 2)
+        grid.addWidget(self.b_inject, 5, 2)
 
         self.b_extract = CXPushButton('Extract', cname='cxhw:0.ddm.extract')
-        grid.addWidget(self.b_extract, 2, 3)
+        grid.addWidget(self.b_extract, 5, 3)
 
         self.b_round = CXPushButton('Round', cname='cxhw:0.ddm.nround')
-        grid.addWidget(self.b_round, 3, 0)
+        grid.addWidget(self.b_round, 6, 0)
 
         self.b_auto = CXPushButton('Auto', cname='cxhw:0.ddm.autorun')
-        grid.addWidget(self.b_auto, 3, 1)
+        grid.addWidget(self.b_auto, 6, 1)
 
         self.b_stop = CXPushButton('Stop', cname='cxhw:0.ddm.stop')
-        grid.addWidget(self.b_stop, 3, 2)
+        grid.addWidget(self.b_stop, 6, 2)
 
 
 class InjExtState(BaseGridW):
